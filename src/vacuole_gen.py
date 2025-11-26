36f71205f380351a73bf9c4977d1e7d4774ac271
Steven Backues <sbackues@emich.edu>
2025-11-26 19:56:10 +0000

    shifted slice position to positive octant and added df_check.csv output for testing purposes (remove later).  Works for 1 body, though Y and Z may not be shifted; throws error for multiple bodies.

--- src/vacuole_gen.py
+++ src/vacuole_gen.py
@@ -340,6 +340,7 @@ def genBalls3(bodies=20, wall_Radius_Mu=6.8, wall_Radius_Sigma=0.34, mu=5, sigma
 
     vacRadOuter = 1.05*vacRadInner
     # Done with checking, now shift everything:
+    zoffset_shifted = zoffset + vacRadOuter
     pos_array_shifted_to_1st_octant = pos_array_shifted_to_origin + vacRadOuter
     r_and_pos_array = np.hstack((np.expand_dims(r,axis=1),pos_array_shifted_to_1st_octant))
 
@@ -350,7 +351,7 @@ def genBalls3(bodies=20, wall_Radius_Mu=6.8, wall_Radius_Sigma=0.34, mu=5, sigma
     vac_r_and_pos = np.hstack((vacRadOuter,vac_pos_array)) # we need to record vacRadOuter not vacRadInner to make sure that it all fits in the positive octant
     r_and_pos_array_w_vac = np.vstack((vac_r_and_pos,r_and_pos_array))
     pos_array = np.delete(r_and_pos_array_w_vac,0,axis=1) # copy everything except 1st col, which is r.
-    d = {'bodynum': -100, 'bodyType':'Vacuole', 'rOuter': vacRadOuter,'p':vacp, 'rInner':vacRadInner, 'slicePosition':zoffset} 
+    d = {'bodynum': -100, 'bodyType':'Vacuole', 'rOuter': vacRadOuter,'p':vacp, 'rInner':vacRadInner, 'slicePosition':zoffset_shifted} 
     df_just_vac = pd.DataFrame(data=[d])
     #MV Pandas Concatenation of Vac Parameter and Dataframe
     df = pd.concat([df_just_vac,df],ignore_index=True)
@@ -360,6 +361,7 @@ def genBalls3(bodies=20, wall_Radius_Mu=6.8, wall_Radius_Sigma=0.34, mu=5, sigma
   dfpos.rename(columns={0: 'x', 1: 'y', 2: 'z'}, inplace=True)
   # add the resulting columns to the main dataframe:
   df = pd.concat([df.reset_index(drop=True), dfpos], axis=1)
+  df.to_csv('df_check.csv')
 
   return(df,pos_array,r_and_pos_array,dirmat_safe,iterCount, ofv_original, ofv_final, compactness)
 
