17762fb9f3e0dc8b1c432621e9f14696809bf502
StevenBackues <sbackues@emich.edu>
2026-05-08 16:12:12 -0400

    Fixed error in loadDataNumber (AVSStats.py) that was causing the empty slices in the simulated data to be omitted.

--- src/AVSStats.py
+++ src/AVSStats.py
@@ -192,11 +192,16 @@ def loadDataNumber(fileSelectOpt):
                     for number_sigma in number_sigmas:
                         split_slices = split_data3.loc[split_data3['number_sigma'] == number_sigma]
                         split_slices = split_slices[sim_slices.time != 'time']  #Removing non-number rows (left-over headers).  This works 
-                        split_slices = split_slices.dropna( axis = 0)   #Removing rows that had "NaN" because there were no bodies captured in that slice
-                        empty_slice_num = split_slices.shape[0] - split_slices.shape[0]   #Calculating the number of rows that had "NaN" because there were no bodies captured in that slice
+                        split_slices.to_csv(os.path.join(directory, f"split_slices_number_mu{number_mu}_sigma{number_sigma}.csv"), index = False)  #Saving the split slices to a csv file for verification
+                        split_slices_noNaN = split_slices.dropna( axis = 0)   #Removing rows that had "NaN" because there were no bodies captured in that slice
+                        split_slices_noNaN.to_csv(os.path.join(directory, f"split_slices_noNaN_number_mu{number_mu}_sigma{number_sigma}.csv"), index = False)  #Saving the split slices with no NaN's to a csv file for verification #For verification
+                        empty_slice_num = split_slices.shape[0] - split_slices_noNaN.shape[0]   #Calculating the number of rows that had "NaN" because there were no bodies captured in that slice
+                        print("empty slice num", empty_slice_num)  #For verification
                         empty_slices = [0]*empty_slice_num      #Creating a list of 0's to represent the empty slices
-                        sim_body_number = pd.DataFrame({'number': split_slices['time'].value_counts().to_list()})  #The number of unique timestamps is the number of bodies in the slice                        sim_body_number = pd.concat([sim_body_number, pd.DataFrame({'number' : empty_slices })], ignore_index = True)   #Adding in the rows for the empty slices
+                        print ("empty slices", empty_slices)  #For verification
+                        sim_body_number = pd.DataFrame({'number': split_slices['time'].value_counts().to_list()})  #Each unique timestamp is a slice
                         sim_body_number = pd.concat([sim_body_number, pd.DataFrame({'number' : empty_slices })], ignore_index = True)   #Adding in the rows for the empty slices
+                        sim_body_number.to_csv(os.path.join(directory, f"sim_body_number_number_mu{number_mu}_sigma{number_sigma}.csv"), index = False)  #Saving the simulated body numbers to a csv file for verification
                         size_mu_list = [float(size_mu)]*len(sim_body_number)  #Creating a list of the size_mu value to add to the dataframe
                         size_sigma_list = [float(size_sigma)]*len(sim_body_number)
                         number_mu_list = [float(number_mu)]*len(sim_body_number)  
